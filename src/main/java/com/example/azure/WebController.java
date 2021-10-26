6e653c581b86edace806ea7b5839bf419f51dd2f
acs507 <eric.eichholtz@haw-hamburg.de>
2021-10-26 22:35:13 +0200

    2

--- src/main/java/com/example/azure/WebController.java
+++ src/main/java/com/example/azure/WebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 //@Controller
 @RestController
 public class WebController {
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     public String showHelloWorld(){
         return "Hallo Welt!";
     }
